29895fd00a2b8cccb527682cf8cd5931cdd3f461
Caen De Silva <caen@desilva.se>
2024-02-04 11:41:29 +0100

    Refactor

--- gh-apply.php
+++ gh-apply.php
@@ -10,60 +10,77 @@
 
 declare(strict_types=1);
 
-if (! isset($argv[1])) {
-    echo 'No input provided';
-    exit(1);
+function getInput(): string
+{
+    if (! isset($argv[1])) {
+        echo 'No input provided';
+        exit(1);
+    }
+
+    // Normalize the input
+    $input = $argv[1];
+
+    $input = trim($input);
+    $input = trim($input, '/');
+
+    // Rewrite http to https
+    $input = str_replace('http://', 'https://', $input);
+
+    // If not ends with .patch, append it
+    if (! str_ends_with($input, '.patch')) {
+        $input .= '.patch';
+    }
+
+    // If string just starts with github.com, prepend https://
+    if (str_starts_with($input, 'github.com')) {
+        $input = 'https://'.$input;
+    }
+
+    // If not starts https://github.com, prepend it
+    if (! str_starts_with($input, 'https://github.com/')) {
+        $input = 'https://github.com/'.$input;
+    }
+
+    // If it does not contain /commit/, inject that before the commit hash (which we find before the last /)
+    if (! str_contains($input, '/commit/')) {
+        $lastSlash = strrpos($input, '/');
+        $input = substr($input, 0, $lastSlash).'/commit/'.substr($input, $lastSlash + 1);
+    }
+
+    return $input;
 }
 
-// Normalize the input
-$input = $argv[1];
+function applyPatch(string $patch): int
+{
+    $patchFile = tempnam(sys_get_temp_dir(), 'patch');
 
-$input = trim($input);
-$input = trim($input, '/');
+    file_put_contents($patchFile, $patch);
 
-// Rewrite http to https
-$input = str_replace('http://', 'https://', $input);
+    $command = 'git apply '.escapeshellarg($patchFile);
 
-// If not ends with .patch, append it
-if (! str_ends_with($input, '.patch')) {
-    $input .= '.patch';
+    passthru($command, $exitCode);
+
+    unlink($patchFile);
+
+    return $exitCode;
 }
 
-// If string just starts with github.com, prepend https://
-if (str_starts_with($input, 'github.com')) {
-    $input = 'https://' . $input;
+function getPatch(string $input): string
+{
+    $patch = file_get_contents($input);
+
+    if ($patch === false) {
+        echo 'Failed to download the patch file';
+        exit(1);
+    }
+
+    return $patch;
 }
 
-// If not starts https://github.com, prepend it
-if (! str_starts_with($input, 'https://github.com/')) {
-    $input = 'https://github.com/' . $input;
-}
+$input = getInput();
 
-// If it does not contain /commit/, inject that before the commit hash (which we find before the last /)
-if (! str_contains($input, '/commit/')) {
-    $lastSlash = strrpos($input, '/');
-    $input = substr($input, 0, $lastSlash) . '/commit/' . substr($input, $lastSlash + 1);
-}
+$patch = getPatch($input);
 
-// Download the patch file
-
-$patch = file_get_contents($input);
-
-if ($patch === false) {
-    echo 'Failed to download the patch file';
-    exit(1);
-}
-
-// Apply the patch
-
-$patchFile = tempnam(sys_get_temp_dir(), 'patch');
-
-file_put_contents($patchFile, $patch);
-
-$command = 'git apply ' . escapeshellarg($patchFile);
-
-passthru($command, $exitCode);
-
-unlink($patchFile);
+$exitCode = applyPatch($patch);
 
 exit($exitCode);
